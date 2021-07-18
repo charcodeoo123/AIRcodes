336f44618eb35b77ce7a47aed7e4b29806161f77
charles <74268804+charcodeoo123@users.noreply.github.com>
2021-07-18 09:43:23 -0700

    testing comm between ard-pi
    may be stupid to read from csv and then send it to ard, given that temper log is already in csv

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+# ls /dev/tty* to find serial port for arduino
+# python3 -m pip install pyserial
+# sudo apt install python3-pip
+
+#!/usr/bin/env python3
+import serial
+import time
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    ser.flush()
+
+    while True:
+        temper =0
+        file = open('my_file.csv', 'r')
+        temper = file.read ()
+        file.close()
+        ser.write("Hello from Raspberry Pi!\n".encode('utf-8')) #testing
+        ser.write(temper+"\n".encode('utf-8'))
+        
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(1)
 
